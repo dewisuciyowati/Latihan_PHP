98305d13ce8e685e80007c8459beaf4f6b16d5d6
dewisuciyowati <dewisuciyowati@gmail.com>
2025-10-08 13:14:26 +0700

    acara 12

--- Array Multidimensi.php
+++ Array Multidimensi.php
@@ -29,13 +29,16 @@ $p["stok"] . "<br>";
 <?php
 // Array 3 Dimensi
 $array = [
-[ // Tabel 1
-[elemen111, elemen112, elemen113],
-[elemen121, elemen122, elemen123]
-],
-[ // Tabel 2
-[elemen211, elemen212, elemen213],
-[elemen221, elemen222, elemen223]
-]
+    [ // Tabel 1
+        ["elemen111", "elemen112", "elemen113"],
+        ["elemen121", "elemen122", "elemen123"]
+    ],
+    [ // Tabel 2
+        ["elemen211", "elemen212", "elemen213"],
+        ["elemen221", "elemen222", "elemen223"]
+    ]
 ];
+
+// Contoh menampilkan salah satu elemen:
+echo $array[0][1][2]; // hasil: elemen123
 ?>
